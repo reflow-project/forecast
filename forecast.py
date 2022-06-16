6c7e78e72114c428294c7a3aaa8c979c0317db05
Stefano Bocconi <Stefano.Bocconi@gmail.com>
2022-06-16 11:16:34 +0200

    Adding vector AR, results still bad

--- forecast.py
+++ forecast.py
@@ -10,14 +10,14 @@ from pandas import Series
 import statsmodels.api as sm
 from statsmodels.tsa.stattools import adfuller, acf, pacf
 from statsmodels.tsa.arima.model import ARIMA
-from statsmodels.tsa.api import SimpleExpSmoothing 
+from statsmodels.tsa.api import SimpleExpSmoothing, VAR
 from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 
 
 from pmdarima import auto_arima
 
 data_dir = '.'
-TRAIN_PERC = .9
+TRAIN_PERC = .8
 
 def df_test(resource, time_series):
     # Advanced Dickey-Fuller Test for stationariety
@@ -120,36 +120,50 @@ def read_data():
 def main():
     purchased, sold, shelf_life = read_data()
     sold.index = sold.datetime
+    sold.drop(['unit','resource_id','datetime'],axis=1, inplace=True)
     purchased.index = purchased.datetime
-    
+    purchased.drop(['unit','resource_id','datetime'],axis=1, inplace=True)
+
+    #################################################
+    ###  Histogram of purchased and sold resources
+    #################################################
     tot_purchased = purchased.groupby('resource_name').purchased.sum()
     tot_sold = sold.groupby('resource_name').sold.sum()
     totals = pd.concat([tot_sold,tot_purchased], axis=1)
     totals.loc[pd.isna(totals.purchased),'purchased'] = 0
     totals.loc[pd.isna(totals.sold),'sold'] = 0
 
-    # breakpoint()
+    # Plot settings
     X_axis = np.arange(len(totals))
     width = 0.25
+    # create a figure to show plots on different windows in a non-blocking manner
     solpur_fig = plt.figure(num='solpur')
     ax1 = solpur_fig.add_subplot(1,1,1)
     ax1.bar(X_axis-width/2, totals.purchased, width=width, color='r', label='purchased')
     ax1.bar(X_axis+width/2, totals.sold, width=width, color='g', label='sold')
-    # Adding legend
     
-  
     plt.xticks(X_axis, totals.index, rotation='vertical')
     plt.xlabel("Resource")
     plt.ylabel("Kg")
     plt.title("Resources sold and purchased")
-
-    # To load the display window
+    # Show but don't block
     solpur_fig.show()
-    # breakpoint()
+    
+    #################################################
+    ## Fit a Vector AR
+    #################################################
+    sold_allres =  sold.set_index('resource_name',append=True).unstack().resample('D').sum()
+    model = VAR(sold_allres)
+    model_fit = model.fit(maxlags=5)
+    model_fit.summary()
+    
+    #################################################
+    ###  Loop on all sold resources
+    #################################################
     for resource in sold['resource_name'].unique():
         # select and resample per day
         sold_resource = sold.loc[sold['resource_name']==resource].resample('D').sum()
-        
+
         solres_fig = plt.figure(num=resource)
         ax1 = solres_fig.add_subplot(1,1,1)
         ax1.plot(sold_resource['sold'], ".-", color='g', label = resource)
@@ -159,6 +173,7 @@ def main():
         plt.title(f'Daily {resource} sold', fontsize=16)
         solres_fig.show()
    
+        # Plot autocorrellation and partial autocorrelation
         acf_plot = plot_acf(sold_resource.sold)
         plt.title(f'{resource} autocorrelation', fontsize=16)
         acf_plot.show()
@@ -166,17 +181,23 @@ def main():
         plt.title(f'{resource} partial autocorrelation', fontsize=16)
         pacf_plot.show()
 
+        # Plot seasonal decomposition
         seasonal_plot = sm.tsa.seasonal_decompose(sold_resource.sold).plot()
         seasonal_plot.show()
+        
+        # Splitting train and test data
         data_len = len(sold_resource)
         y_to_train = sold_resource[:int(data_len*TRAIN_PERC)].squeeze(axis=1)
         y_to_test = sold_resource[int(data_len*TRAIN_PERC):].squeeze(axis=1)
+        print(f'predicting {len(y_to_test)} days')
         
+        # Test if data is stationary
         if df_test(resource, sold_resource):
+            
             ses_fit = ses(y_to_train)
             ses_fcast = ses_fit.forecast(len(y_to_test))
             mse2 = ((ses_fcast - y_to_test) ** 2).mean()
-            print(f"SES RMS Error with auto optimization (smoothing_level = {ses_fit.model.params['smoothing_level']}) is {round(np.sqrt(mse2), 2)}")
+            print(f"SES RMS Error (smoothing = {ses_fit.model.params['smoothing_level']}) is {round(np.sqrt(mse2), 2)}")
             ses_all = pd.concat([ses_fit.predict(), ses_fcast])
             ax1.plot(ses_all, ".-", color='r', label = 'ses')
             
@@ -187,37 +208,20 @@ def main():
             arima_all = pd.concat([arima_fit.predict(), arima_fcast])
             ax1.plot(arima_all, ".-", color='b', label = 'arima')
 
-            sarima_fit = sarima(y_to_train,3)
-            sarima_fcast = sarima_fit.forecast(len(y_to_test))
-            mse2 = ((sarima_fcast - y_to_test) ** 2).mean()
-            print(f"SARIMA RMS Error is {round(np.sqrt(mse2), 2)}")
-            sarima_all = pd.Series(index=arima_all.index, data=np.concatenate((sarima_fit.predict(),sarima_fcast)))
-            ax1.plot(sarima_all, ".-", color='y', label = 'sarima')
-            
-
-
-        else:
-            model = auto_arima(sold_resource.sold,
-                        start_p=0,
-                        start_q=0,
-                        max_p=5,
-                        max_q=5,
-                        d=None,
-                        D=None,
-                        max_d=5,
-                        start_P=0,
-                        start_Q=0,
-                        max_P=5,
-                        max_D=5,
-                        max_Q=5,
-                        trace=True)
-            print(model.summary())
+        sarima_fit = sarima(y_to_train,7)
+        sarima_fcast = sarima_fit.forecast(len(y_to_test))
+        mse2 = ((sarima_fcast - y_to_test) ** 2).mean()
+        print(f"SARIMA RMS Error is {round(np.sqrt(mse2), 2)}")
+        sarima_all = pd.Series(index=sold_resource.index, data=np.concatenate((sarima_fit.predict(),sarima_fcast)))
+        ax1.plot(sarima_all, ".-", color='y', label = 'sarima')
+        
         
         ax1.legend(loc = 'best')
         plt.show()
         plt.close(fig=solres_fig)
         plt.close(fig=acf_plot)
         plt.close(fig=pacf_plot)
+        plt.close(fig=seasonal_plot)
 
         # breakpoint()
     
